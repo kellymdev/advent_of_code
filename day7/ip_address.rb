7af95ff25c3c9f98a638dbbb297bb317677877c4
Kelly Munro <kellymdev@gmail.com>
2016-12-07 19:55:05 +1300

    Day 7 part 1

---
+++ day7/ip_address.rb
@@ -0,0 +1,84 @@
+class IpAddress
+  def initialize(address_list)
+    @address_list = format_address_list(address_list)
+    @abba_list = create_abba_list
+    @tls_count = 0
+  end
+
+  def run
+    @address_list.each do |address|
+      if supports_tls?(address)
+        @tls_count += 1
+      end
+    end
+
+    print_tls_count
+  end
+
+  private
+
+  def abba_in_square_brackets?(address)
+    hypernet = ""
+    brace_count = number_of_opening_square_braces(address)
+
+    sub_addresses = address.split("[")
+
+    sub_addresses[1..-1].each do |subaddress|
+      hypernet = subaddress.split("]").first
+
+      return true if contains_abba?(hypernet)
+    end
+
+    false
+  end
+
+  def number_of_opening_square_braces(address)
+    brace_count = 0
+
+    address.chars.each do |char|
+      if opening_square_brace?(char)
+        brace_count += 1
+      end
+    end
+
+    brace_count
+  end
+
+  def opening_square_brace?(char)
+    char == "["
+  end
+
+  def print_tls_count
+    puts "#{@tls_count}"
+  end
+
+  def supports_tls?(address)
+    contains_abba?(address) && !abba_in_square_brackets?(address)
+  end
+
+  def contains_abba?(address)
+    @abba_list.each do |abba|
+      return true if address.include?(abba)
+    end
+
+    false
+  end
+
+  def create_abba_list
+    list = []
+
+    ("a".."z").each do |letter|
+      ("a".."z").each do |char|
+        if letter != char
+          list << "#{letter}#{char}#{char}#{letter}"
+        end
+      end
+    end
+
+    list
+  end
+
+  def format_address_list(address_list)
+    address_list.split("\n")
+  end
+end
