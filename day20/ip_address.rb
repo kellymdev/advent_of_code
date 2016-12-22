056ad79e78c6f7fce43fc12f16028c5906ca6f85
Kelly Munro <kelly.munro@powershop.co.nz>
2016-12-22 14:38:12 +1300

    Day 20 part 1

--- day20/ip_address.rb
+++ day20/ip_address.rb
@@ -3,25 +3,71 @@ class IpAddress
     @blacklist = format_blacklist(blacklist)
     @minimum_value = minimum_value
     @maximum_value = maximum_value
+    @current_minimum = nil
+    @current_maximum = nil
     @blacklisted_values = []
+    @lowest_ip = 0
   end
 
   def run
     return unless starts_with_zero?(@blacklist.first)
 
-    @blacklist.each do |range|
+    @blacklist.each_with_index do |range, index|
+      puts "Processing record #{index}"
+
       start_value = start_value(range)
       end_value = end_value(range)
 
-      @blacklisted_values += (start_value..end_value).to_a
-
-      @blacklisted_values.uniq
+      @blacklisted_values << [start_value, end_value]
+      @blacklisted_values.sort!
     end
 
+    @blacklisted_values.each do |value|
+      lowest = value[0]
+      highest = value[1]
 
+      update_current_minimum(lowest) if @current_minimum.nil?
+      update_current_maximum(highest) if @current_maximum.nil?
+
+      if value_included_in_range?(lowest) || consecutive_value_for_maximum == lowest
+        if !value_included_in_range?(highest)
+          update_current_maximum(highest)
+        end
+      else
+        @lowest_ip = consecutive_value_for_maximum
+
+        break
+      end
+    end
+
+    print_lowest_ip
   end
 
   private
+
+  def update_current_minimum(value)
+    @current_minimum = value
+  end
+
+  def update_current_maximum(value)
+    @current_maximum = value
+  end
+
+  def consecutive_value_for_maximum
+    @current_maximum + 1
+  end
+
+  def consecutive_value_for_minimum
+    @current_minimum - 1
+  end
+
+  def value_included_in_range?(value)
+    (@current_minimum..@current_maximum).include?(value)
+  end
+
+  def print_lowest_ip
+    puts "Lowest ip: #{@lowest_ip}"
+  end
 
   def starts_with_zero?(range)
     start_value(range).zero?
