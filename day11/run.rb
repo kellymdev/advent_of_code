343dbbc16e6cc85d5f233f7f1acf621d2f29ab10
Kelly Munro <kelly.munro@powershop.co.nz>
2016-12-22 16:51:09 +1300

    WIP day 11

--- day11/run.rb
+++ day11/run.rb
@@ -1,18 +1,18 @@
 require_relative 'radioisotope.rb'
 
-# input = <<-STRING
-# The first floor contains a hydrogen-compatible microchip, a lithium-compatible microchip.
-# The second floor contains a hydrogen generator.
-# The third floor contains a lithium generator.
-# The fourth floor contains nothing relevant.
-# STRING
-
 input = <<-STRING
-The first floor contains a thulium generator, a thulium-compatible microchip, a plutonium generator, a strontium generator.
-The second floor contains a plutonium-compatible microchip, a strontium-compatible microchip.
-The third floor contains a promethium generator, a promethium-compatible microchip, a ruthenium generator, a ruthenium-compatible microchip.
+The first floor contains a hydrogen-compatible microchip, a lithium-compatible microchip.
+The second floor contains a hydrogen generator.
+The third floor contains a lithium generator.
 The fourth floor contains nothing relevant.
 STRING
+
+# input = <<-STRING
+# The first floor contains a thulium generator, a thulium-compatible microchip, a plutonium generator, a strontium generator.
+# The second floor contains a plutonium-compatible microchip, a strontium-compatible microchip.
+# The third floor contains a promethium generator, a promethium-compatible microchip, a ruthenium generator, a ruthenium-compatible microchip.
+# The fourth floor contains nothing relevant.
+# STRING
 
 radioisotope = Radioisotope.new(input)
 radioisotope.run
